0fe29e46dc392cec9dae7340f8b03ceb109dd197
Andrew Herculano <andrewherculano@hotmail.com>
2020-11-19 23:59:31 -0300

    Add check situation

--- calculador-imc/scripts/mainpage.js
+++ calculador-imc/scripts/mainpage.js
@@ -1,16 +1,44 @@
 const display = document.getElementById("display");
 const pesoInput = document.getElementById("peso-atual");
 const alturaInput = document.getElementById("altura");
-
-//console.log(display.textContent);
+const nomeInput = document.getElementById("nome");
 
 calculateImc = () => {
   const peso = parseFloat(pesoInput.value.replace(",", "."));
   const altura = parseFloat(alturaInput.value.replace(",", "."));
+  const nome = nomeInput.value;
 
-  var imcResult = peso / (altura * altura);
+  const imcResult = peso / (altura * altura);
 
-  display.textContent = imcResult.toFixed(2);
+  if (imcResult <= 17) {
+    display.textContent = `${nome} seu IMC atual é de: ${imcResult.toFixed(
+      2
+    )}, Magreza grave`;
+  } else if (imcResult <= 18.5) {
+    display.textContent = `${nome} seu IMC atual é de: ${imcResult.toFixed(
+      2
+    )}, Magreza leve`;
+  } else if (imcResult <= 25) {
+    display.textContent = `${nome} seu IMC atual é de: ${imcResult.toFixed(
+      2
+    )}, Saudável`;
+  } else if (imcResult <= 30) {
+    display.textContent = `${nome} seu IMC atual é de: ${imcResult.toFixed(
+      2
+    )}, Sobrepeso`;
+  } else if (imcResult <= 35) {
+    display.textContent = `${nome} seu IMC atual é de: ${imcResult.toFixed(
+      2
+    )}, Obesidade Grau I`;
+  } else if (imcResult <= 40) {
+    display.textContent = `${nome} seu IMC atual é de: ${imcResult.toFixed(
+      2
+    )}, Obesidade Grau II (considerada severa)`;
+  } else {
+    display.textContent = `${nome} seu IMC atual é de: ${imcResult.toFixed(
+      2
+    )}, Obesidade Grau III (considerada mórbida`;
+  }
 };
 
 document.getElementById("calculate").addEventListener("click", calculateImc);
